0a3a6c646fcfad7463668516c4a6f50a844c9d46
Matt Masurka <m.masurka@gmail.com>
2020-02-28 21:28:28 -0800

    Finished

--- coinChange-02-26-20.py
+++ coinChange-02-26-20.py
@@ -1,26 +1,26 @@
 # https://leetcode.com/problems/coin-change/
 
 def coinChange(coins, amount):
+  coins = sorted(coins)
   table = [[x for x in range(amount + 1)] for y in range(len(coins))]
   
   for coinIdx in range(len(coins)):
     coin = coins[coinIdx]
     for a, amount in enumerate(table[coinIdx]):
-      minimum = 0
+      minimum = -1
       remainder = amount % coin
       if amount < coin:
         minimum = table[coinIdx - 1][a] if table[coinIdx - 1][a] is not None else 0
       elif remainder == 0:
         minimum = amount // coin
-      else:
-        remainderMinCoins = table[coinIdx - 1][a - remainder]
+      elif coinIdx >= 1:
+        remainderMinCoins = table[coinIdx - 1][remainder]
         floorDivisionMinCoins = amount // coin
         minimum = remainderMinCoins + floorDivisionMinCoins
-        print('coin: ', coin, '; amount: ', amount, '; remainderMinCoins: ', remainderMinCoins, '; floorDivisionMinCoins: ', floorDivisionMinCoins)
       table[coinIdx][a] = minimum
-  print(table)
   return table[-1][-1]
 
 
-result = coinChange([1, 2, 5], 10)
+# result = coinChange([1, 2, 5], 11)
+result = coinChange([3, 5, 1], 23)
 print(result)
